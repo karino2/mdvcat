a054fdc0728ae0a253b28cc4cdb2a22d27e5defa
Kazuma Arino <hogeika2@gmail.com>
2022-01-30 21:58:43 +0900

    Wrongly self-contained while runtime is also installed.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-dotnet publish -c release -r osx-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:PublishTrimmed=true
+dotnet publish -c release -r osx-x64 --self-contained false /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 cp bin/release/net5.0/osx-x64/publish/mdvcat ~/bin
